4994fe9d7e1d6c6572ac400f8f581ceafe404712
borkowski <borkowski@enchantaffiliates.com>
2024-10-17 14:00:39 +0300

    add content

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,62 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <>Nail color generator</>;
+  const [color, setColor] = useState("rgb(255, 0, 0)");
+  const [shouldGenerate, setShouldGenerate] = useState(false);
+
+  const [countColor, setCountColor] = useState(0);
+  const [speed, setSpeed] = useState(30);
+
+  const generateColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+
+    setColor(`rgb(${r}, ${g}, ${b})`);
+  };
+
+  useEffect(() => {
+    const generatingColors = setTimeout(() => {
+      if (countColor < 100 && shouldGenerate) {
+        generateColor();
+        setCountColor((prev) => prev + 1);
+      }
+    }, speed);
+    return () => clearTimeout(generatingColors);
+  }, [countColor, shouldGenerate]);
+
+  useEffect(() => {
+    if (countColor === 100) {
+      setShouldGenerate(false);
+      setCountColor(0);
+    }
+    setSpeed(() => {
+      if (countColor > 95) {
+        return 500;
+      }
+      return 30;
+    });
+  }, [countColor]);
+
+  return (
+    <div className="wrapper">
+      <div className="top-content">
+        <h1>Генератор цвета ногтей для моей малышки</h1>
+        <div style={{ background: color }} className="item"></div>
+        <button
+          onClick={() => setShouldGenerate(true)}
+          className="generate-button"
+        >
+          Жмяк
+        </button>
+      </div>
+      <div>
+        <h2>Сохраненные цвета:</h2>
+        <p>Пупс еще не сохранял цвета</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
