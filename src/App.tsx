361c6b54de2475458c7e9a776deb764400c6ecd8
borkowski <borkowski@enchantaffiliates.com>
2024-10-17 15:16:27 +0300

    add content

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,53 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import uuid from "react-uuid";
+
+interface ISavedColors {
+  color: string;
+  id: string;
+}
 
 function App() {
   const [color, setColor] = useState("rgb(255, 0, 0)");
-  const [shouldGenerate, setShouldGenerate] = useState(false);
-
   const [countColor, setCountColor] = useState(0);
   const [speed, setSpeed] = useState(30);
+  const [shouldSaveColor, setShouldSaveColor] = useState(false);
+  const [shouldGenerate, setShouldGenerate] = useState(false);
+  const [isAnimateButton, setIsAnimateButton] = useState(true);
+  const [savedColors, setSavedColors] = useState<ISavedColors[]>([]);
 
   const generateColor = () => {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
-
     setColor(`rgb(${r}, ${g}, ${b})`);
   };
+
+  const setShouldGenerateHandler = () => {
+    setShouldGenerate(true);
+    setIsAnimateButton(false);
+  };
+
+  const saveColorHandler = () => {
+    setSavedColors((prev) => {
+      const result = [...prev, { color, id: uuid() }];
+      localStorage.setItem("savedColors", JSON.stringify(result));
+      return result;
+    });
+    setShouldSaveColor(false);
+  };
+
+  const deleteColorHandler = (id: string) => {
+    const filteredSavedColors = savedColors.filter((el) => el.id !== id);
+    setSavedColors(filteredSavedColors);
+    localStorage.setItem("savedColors", JSON.stringify(filteredSavedColors));
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("savedColors") !== null) {
+      setSavedColors(JSON.parse(localStorage.getItem("savedColors")!));
+    }
+  }, []);
 
   useEffect(() => {
     const generatingColors = setTimeout(() => {
@@ -30,6 +63,8 @@ function App() {
     if (countColor === 100) {
       setShouldGenerate(false);
       setCountColor(0);
+      setIsAnimateButton(true);
+      setShouldSaveColor(true);
     }
     setSpeed(() => {
       if (countColor > 95) {
@@ -42,18 +77,36 @@ function App() {
   return (
     <div className="wrapper">
       <div className="top-content">
-        <h1>Генератор цвета ногтей для моей малышки</h1>
+        <h1>Не знаешь в какой цвет покрасить ноготки? Нажми на глазик 😘</h1>
         <div style={{ background: color }} className="item"></div>
         <button
-          onClick={() => setShouldGenerate(true)}
-          className="generate-button"
+          onClick={saveColorHandler}
+          className={`shouldSaveColorButton ${shouldSaveColor ? "show" : ""}`}
         >
-          Жмяк
+          Сохранить сгенерированный цвет?
         </button>
+        <button
+          onClick={setShouldGenerateHandler}
+          className={`generate-button ${isAnimateButton ? "animation" : ""}`}
+        ></button>
       </div>
-      <div>
+      <div className="saved-colors-wrapper">
         <h2>Сохраненные цвета:</h2>
-        <p>Пупс еще не сохранял цвета</p>
+        <div className="saved-colors">
+          {savedColors.length > 0 ? (
+            savedColors.map(({ color, id }) => (
+              <div
+                style={{ background: color }}
+                className="item"
+                onClick={() => deleteColorHandler(id)}
+              >
+                Кликни чтобы удалить
+              </div>
+            ))
+          ) : (
+            <p>Пупс еще не сохранял цвета 😢 </p>
+          )}
+        </div>
       </div>
     </div>
   );
